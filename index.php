dd75b75c4634c3bf58f27a9585ed451b4bcf71a9
Unknown <benjamincaillet14@gmail.com>
2018-04-04 11:48:38 +0200

    CSS update #1
    CSS on index only

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
  <?php include("header.php"); ?>
- 
-  <div class="container-fluid">
+
+  <div id="home" class="container-fluid">
       <a href="login.php"><button type="button" class="btn btn-primary" >Sign in</button></a>
       <a href="register.php"><button type="button" class="btn btn-secondary" href="register.php">Sign Up</button></a>
   </div>
