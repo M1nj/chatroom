6c840b20bcdfb4591b162a084ad0cd9c98c7c39f
Unknown <benjamincaillet14@gmail.com>
2018-04-04 12:58:43 +0200

    Update Database

--- chatroom.sql
+++ chatroom.sql
@@ -2,8 +2,8 @@
 -- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost
--- Généré le :  mar. 03 avr. 2018 à 09:53
+-- Hôte : localhost:8889
+-- Généré le :  mer. 04 avr. 2018 à 10:56
 -- Version du serveur :  5.6.38
 -- Version de PHP :  7.2.1
 
@@ -19,6 +19,13 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `chatroom`
 --
+
+--
+-- Déchargement des données de la table `user_base`
+--
+
+INSERT INTO `user_base` (`id_user`, `nickname`, `mail`, `password`, `profile_picture`, `name_theme`) VALUES
+(5, 'Raikes', 'benjamincaillet14@gmail.com', 'Test', '0', '0');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
