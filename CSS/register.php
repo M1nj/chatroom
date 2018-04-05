5136f81efdad1847a81920755ca3a78d2de5d3e0
Unknown <clemence.rend@gmail.com>
2018-04-05 13:57:44 +0200

    CSS for sign up
    Change CSS for sign up page

--- CSS/register.php
+++ CSS/register.php
@@ -103,35 +103,30 @@
 
 
 <!DOCTYPE html>
-<p>
-<br />
-    <img src="IMG/logo.png" alt="LogoChitChat" />
-</p>
+
+<div class="container">
+    <a href="index.php"><button class="btn btn-warning">Back to the homepage</button></a>
+</div>
+
     <div class="container">
-    <h1 class="title">Welcome !</h1>
         <form class="form register" method="post" action="register.php">
-        <p>Fill in the form to be part of our community<p>
   <div class="form-group">
     <input type="text" class="form-control" name="nickname" placeholder="Pick your nickname">
   </div>
   <div class="form-group">
     <input type="email" class="form-control" id="exampleInputEmail1" name="mail"aria-describedby="emailHelp" placeholder="Your email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
     <input type="password" class="form-control" name="password"id="exampleInputPassword1" placeholder="Your password">
-  </div>
-  <div class="form-group">
-    <input type="confirmpassword" class="form-control" name="confirmpassword"id="exampleInputPassword1" placeholder="Confirm your password">
   </div>
   <div class="form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">By submitting your profile details, you agree to our terms and conditions.</label>
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-<div class="container">
-    <a href="index.php"><button class="btn btn-warning">Back to the homepage</button></a>
-</div>
 <?php echo $error ?>
     </form>
 </div>
