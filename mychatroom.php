b885a9d1ccf4e28ee214631d82c024ba1dec7312
Unknown <clemence.rend@gmail.com>
2018-04-09 15:44:40 +0200

    Update CSS Mychatroom
    MAJ du css page Mychatroom

--- mychatroom.php
+++ mychatroom.php
@@ -6,10 +6,9 @@
 <body id="body"> 
     <h1>My Chatrooms</h1>  
         <h2>Chatrooms I created</h2>
-
-            <div class="container">
-                <a href="createchatroom.php"><button class="btn-create">Create</button></a>
-
+            <a href="createchatroom.php"><button class="btn-create">Create</button></a>
+        <br>
         <h2>Chatrooms I'm in</h2>
+        
 </body>
 </html>
